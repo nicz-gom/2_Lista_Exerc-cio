2c620e389198eefba3d96895ad0afcf893a2bc9f
Nícolas <161242131+nicz-gom@users.noreply.github.com>
2025-02-01 12:44:53 -0300

    Verificador de números, versão: 2,0

--- script.js
+++ script.js
@@ -3,39 +3,40 @@ let buttonElement = document.querySelector("button");
 let ulElement = document.querySelector("ul");
 
 function processador(){
-    if(inputElement.value === ''){
+    if(inputElement.value.trim() === ''){ //o trim serve para desconsiderar espaços em branco
         alert("Você precisa digitar algum número!");
+        return;
     }else{
         const valor = Number(inputElement.value);
         alert(`Número digitado: ${valor}`);
-        
-        let liElement = document.createElement("li");
-        let resultElement = document.createElement("a");
 
+        ulElement.innerHTML = "";
+
+        let liElement = document.createElement("li");
+
+        let textContent = "";
 
         if(valor == 0){  
-            let text = document.createTextNode(`O valor é igual a zero!`);
-            resultElement.appendChild(text);
-            liElement.appendChild(resultElement);
-            ulElement.appendChild(liElement);
-            ulElement = '';
-            ulElement.innerHTML;
+            textContent = "O valor é igual a zero!";
         }
         if(valor > 0){
-            let text = document.createTextNode(`O valor é positivo!`);
-            resultElement.appendChild(text);
-            liElement.appendChild(resultElement);
-            ulElement.appendChild(liElement);
-            ulElement = '';
-            ulElement.innerHTML;
+            textContent = "O valor é positivo!";
         }
         if(valor < 0){
-            let text = document.createTextNode(`O valor é negativo!`);
-            resultElement.appendChild(text);
-            liElement.appendChild(resultElement);
-            ulElement.appendChild(liElement);
-            ulElement = '';
-            ulElement.innerHTML;
+            textContent = "O valor é negativo!";
         }
+
+        liElement.textContent = textContent;
+        ulElement.appendChild(liElement);
+
+        inputElement.value = '';
+
+        inputElement.focus();
     }
+}
+
+function reload(){
+   addEventListener("click", () => {
+        location.reload();
+   })
 }
